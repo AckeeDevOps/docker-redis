903db2d99b177a455f9d183ccccb62ce78c0806d
Tomáš Hejátko <tomas.hejatko@gmail.com>
2018-12-13 17:41:03 +0100

    setup hostname in Stackdriver config

--- 4.0/setup_stackdriver.sh
+++ 4.0/setup_stackdriver.sh
@@ -3,4 +3,6 @@
 cd /opt/stackdriver/collectd/etc/collectd.d/ 
 curl -O https://raw.githubusercontent.com/Stackdriver/stackdriver-agent-service-configs/master/etc/collectd.d/redis.conf
 
+sed -i "s/mynode/${HOSTNAME}/g" /opt/stackdriver/collectd/etc/collectd.d/redis.conf
+
 service stackdriver-agent restart
